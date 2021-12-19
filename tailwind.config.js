980427684d7514f1696b7fdfd66a26e69285d5d2
✨ Alex ✨ <alex@dytry.ch>
2021-12-19 12:49:56 +0000

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: false,
-    content: ['./src/**/*.{html,js,ts,tsx}'],
-  },
+  content: ['./src/**/*.{html,js,ts,tsx}'],
   theme: {
     screens: {
       xl: '1280px',

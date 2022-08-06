92719025ca8fc0e0a5e081b614304af7079db6bd
alex <alex@dytry.ch>
2022-08-06 14:46:34 +0000

    [automated] publish
    This commit was generated by .github/workflows/publish.yml on Sat Aug  6 14:46:34 UTC 2022.

---
+++ assets/webgl.75dd4854.js
@@ -0,0 +1,31 @@
+import{a as c,t as A,r as i}from"./assert.99a33aa8.js";import{A as _}from"./AABB.a932f068.js";import{V as l}from"./Vector2.550325b5.js";var R=`attribute vec2 a_position;
+attribute vec4 a_color;
+
+varying vec4 v_color;
+
+uniform vec2 u_resolution;
+
+void main() {
+  
+  vec2 zeroToOne = a_position / u_resolution;
+ 
+  
+  vec2 zeroToTwo = zeroToOne * 2.0;
+ 
+  
+  vec2 clipSpace = zeroToTwo - 1.0;
+ 
+  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+
+  
+  
+  
+  v_color = a_color;
+}`,p=`precision mediump float;
+ 
+uniform vec4 u_color;
+varying vec4 v_color;
+
+void main() {
+  gl_FragColor = v_color;
+}`;const r=document.createElement("canvas");r.width=document.body.clientWidth*window.devicePixelRatio;r.height=document.body.clientHeight*window.devicePixelRatio;r.style.width=`${document.body.clientWidth}px`;r.style.height=`${document.body.clientHeight}px`;document.body.appendChild(r);const e=r.getContext("webgl");c(e);var d=(o=>(o[o.Fragment=WebGLRenderingContext.FRAGMENT_SHADER]="Fragment",o[o.Vertex=WebGLRenderingContext.VERTEX_SHADER]="Vertex",o))(d||{});function f(o,a,t){const n=o.createShader(a);if(c(n),o.shaderSource(n,t),o.compileShader(n),!o.getShaderParameter(n,o.COMPILE_STATUS))throw console.log(o.getShaderInfoLog(n)),o.deleteShader(n),new Error("compile error");return n}function b(o,a,t){const n=o.createProgram();if(c(n),o.attachShader(n,a),o.attachShader(n,t),o.linkProgram(n),!o.getProgramParameter(n,o.LINK_STATUS))throw console.log(o.getProgramInfoLog(n)),o.deleteProgram(n),"link error";return n}const w=f(e,d.Vertex,R),S=f(e,d.Fragment,p),s=b(e,w,S),h=e.getAttribLocation(s,"a_position");e.getAttribLocation(s,"a_color");const F=e.getUniformLocation(s,"u_resolution"),m=e.createBuffer();c(m);const T=[10,20,80,20,10,30,10,30,80,20,80,30];e.bindBuffer(e.ARRAY_BUFFER,m);e.bufferData(e.ARRAY_BUFFER,new Float32Array(T),e.STATIC_DRAW);e.viewport(0,0,r.width,r.height);e.clearColor(0,0,0,0);e.clear(e.COLOR_BUFFER_BIT);e.useProgram(s);e.uniform2f(F,e.canvas.width,e.canvas.height);e.enableVertexAttribArray(h);e.bindBuffer(e.ARRAY_BUFFER,m);e.vertexAttribPointer(h,2,e.FLOAT,!1,0,0);const u=100,v=new Float32Array(u*12);function x(o,a,t){v.set([t.left,t.top,t.right,t.top,t.left,t.bottom,t.left,t.bottom,t.right,t.top,t.right,t.bottom],a*12)}A(u,o=>{const a=new _(new l(i(e.canvas.width),i(e.canvas.height)),new l(i(200),i(200)));x(e,o,a)});e.bufferData(e.ARRAY_BUFFER,v,e.STATIC_DRAW);e.drawArrays(e.TRIANGLES,0,u*6);

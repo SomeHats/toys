ed16a958598df70df86ac6f2571bb6fb8d8b80a0
alex <alex@dytry.ch>
2022-08-20 13:33:25 +0000

    [automated] publish
    This commit was generated by .github/workflows/publish.yml on Sat Aug 20 13:33:25 UTC 2022.

--- chunks/chunk_easings.faef6eea.js
+++
@@ -1,1 +0,0 @@
-const s=a=>n=>a(1-n),o=a=>a,t=a=>Math.sin(a/1*(Math.PI*.5)),c=a=>-.5*(Math.cos(Math.PI*a)-1),e=(a=1.70158)=>n=>1*n*n*((a+1)*n-a),r=(a=1.70158)=>n=>(n=n-1,1*(n*n*((a+1)*n+a)+1));export{t as a,c as b,e as i,o as l,r as o,s as r};

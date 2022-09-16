fef09405ef1a79619d2194b50e05ad0eb570876f
✨ Alex ✨ <alex@dytry.ch>
2022-09-16 15:57:45 +0100

    previous line onion skin

--- src/splatapus/renderer/StrokeRenderer.tsx
+++ src/splatapus/renderer/StrokeRenderer.tsx
@@ -27,14 +27,14 @@ export function StrokeRenderer({
     shapeId: SplatShapeId;
     previewPosition: PreviewPosition;
 }) {
-    let centerPoints = null;
+    let toolPoints = null;
     if (previewPosition.selectedShapeId === shapeId) {
         switch (interaction.selectedTool.type) {
             case ToolType.Draw: {
                 const state = DrawTool.getState(interaction.selectedTool);
                 switch (state.state) {
                     case "drawing":
-                        centerPoints = normalizeCenterPointIntervalsQuadratic(
+                        toolPoints = normalizeCenterPointIntervalsQuadratic(
                             getStrokeCenterPoints(
                                 getStrokePoints(state.points, perfectFreehandOpts),
                                 perfectFreehandOpts,
@@ -55,20 +55,34 @@ export function StrokeRenderer({
                 exhaustiveSwitchError(interaction.selectedTool);
         }
     }
-    if (!centerPoints) {
-        centerPoints = interpolationCache.getCenterPointsAtPosition(
-            document,
-            shapeId,
-            previewPosition,
-        );
-    }
+    const centerPoints = interpolationCache.getCenterPointsAtPosition(
+        document,
+        shapeId,
+        previewPosition,
+    );
 
     return (
-        <path
-            d={getSvgPathFromStroke(pathFromCenterPoints(centerPoints))}
-            className={classNames(
-                previewPosition.selectedShapeId === shapeId ? "fill-stone-800" : "fill-stone-600",
+        <>
+            <path
+                d={getSvgPathFromStroke(pathFromCenterPoints(centerPoints))}
+                className={classNames(
+                    toolPoints
+                        ? "fill-stone-300"
+                        : previewPosition.selectedShapeId === shapeId
+                        ? "fill-stone-800"
+                        : "fill-stone-600",
+                )}
+            />
+            {toolPoints && (
+                <path
+                    d={getSvgPathFromStroke(pathFromCenterPoints(toolPoints))}
+                    className={classNames(
+                        previewPosition.selectedShapeId === shapeId
+                            ? "fill-stone-800"
+                            : "fill-stone-600",
+                    )}
+                />
             )}
-        />
+        </>
     );
 }

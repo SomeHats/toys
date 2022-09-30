136d6f5c3c57d01ee9ca00d88eced8a0bd0b0ced
✨ Alex ✨ <alex@dytry.ch>
2022-09-30 18:34:30 +0100

    more live?

--- src/lib/live/LiveMemo.tsx
+++ src/lib/live/LiveMemo.tsx
@@ -2,6 +2,7 @@ import { assert } from "@/lib/assert";
 import { Unsubscribe } from "@/lib/EventEmitter";
 import { Live } from "@/lib/live/live";
 import { LiveComputation, trackRead } from "@/lib/live/LiveComputation";
+import { applyUpdate, UpdateAction } from "@/lib/utils";
 
 const NOT_COMPUTED = Symbol();
 
@@ -30,5 +31,20 @@ export class LiveMemo<T> implements Live<T> {
 
     addInvalidateListener(callback: () => void): Unsubscribe {
         return this.computation.addInvalidateListener(callback);
+    }
+}
+
+export class LiveMemoWritable<T> extends LiveMemo<T> {
+    constructor(read: () => T, private readonly write: (newValue: T) => void) {
+        super(read);
+    }
+
+    set(update: UpdateAction<T>) {
+        // we have to eagerly evaluate memo updates because otherwise we don't know what to invalidate :(
+        const initialValue = this.getWithoutListening();
+        const newValue = applyUpdate(initialValue, update);
+        if (!Object.is(newValue, initialValue)) {
+            this.write(newValue);
+        }
     }
 }
